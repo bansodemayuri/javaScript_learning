99c58871ed77b1aa20588627f54890d955a7ab3f
bansodemayuri <113650944+bansodemayuri@users.noreply.github.com>
2022-10-04 03:43:19 -1200

    added console methods
    added console methods

---
+++ assignment1.js
@@ -0,0 +1,6 @@
+console.log("My name is Mayuri Bansode");
+console.log("My Hobbies : 1.Reading 2.Dreawing 3.Cooking");
+console.info("My strenth : Positive attitude,honestly");
+console.warn("My weakness : Public Speaking, emotional");
+console.log("30+50");
+console.log("49*479");
