a72c50bb046497254a85384309a5719e9583b59e
bansodemayuri <113650944+bansodemayuri@users.noreply.github.com>
2022-10-05 20:00:16 -1200

    01_variable_assignment1
    01_variable_assignment1

---
+++ 01_variable_assignment1.js
@@ -0,0 +1,27 @@
+var myFullName;
+myFullName="Mayuri Bansode";
+   console.log("01.Initial Value-", myFullName);
+
+var myFullName="Mayuri  Mahadev Bansode";
+   console.log("02.Then Updated Value -",myFullName);
+
+var myLove="I love only JS";
+   console.log("03.",myLove);
+   
+var firstName="Mayuri";
+   console.log("First Name :-", firstName);
+
+var lastName="Bansode";
+   console.log("Last Name :-",lastName);
+
+var age="22";
+   console.log("Age :-",age);
+
+var address="A/P- Methwade ,Tal- Sangola, Dist- Solapur.";
+   console.log("Address :-",address);
+
+var pinCode="413317";
+   console.log("Pin Code :-",pinCode);
+   
+var gender="Female";
+   console.log("Gender :-",gender);
---
+++ 01_variable_assignment2.js
@@ -0,0 +1,13 @@
+var my_number;
+my_number=100;
+console.log("my_number");
+
+var my_love;
+my_love="I love only JS";
+console.log("my_love");
+
+var temp;
+temp=200;
+console.log("Before Updating Value of Temp", temp);
+temp="java_script";
+console.log("After Updating Value of temp", temp);
