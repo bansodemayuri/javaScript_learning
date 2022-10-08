f0a4dccd86b3c1866c340752482a8c42002404b3
bansodemayuri <113650944+bansodemayuri@users.noreply.github.com>
2022-10-07 21:20:02 -1200

    02_assignment02
    02_assignment02

---
+++ 02_assignment01.js
@@ -0,0 +1,18 @@
+var Numbe=21;
+var name =" Mayuri";
+var true1=true;
+var pin=411056;
+
+console.log(typeof Number);
+console.log(typeof name);
+console.log(typeof true);
+console.log(typeof pin);
+console.log("==================Null property================");
+var city=null;
+console.log(typeof city);
+console.log("==================Undefined property================");
+
+var my_name;
+console.log(my_name);
+console.log("==========================");
+console.log(typeof my_name);
