a77cb782c53652cbd3aa8542dd5f6339990645e1
bansodemayuri <113650944+bansodemayuri@users.noreply.github.com>
2022-10-09 23:30:33 -1200

    Function Expression
    Function Expression

---
+++ 04_string.js
@@ -0,0 +1,21 @@
+
+var greet = "Good Morning bro and sis";
+
+console.log(greet);
+
+var count = greet.length;
+console.log(count);
+
+console.log(greet.length);
+
+var sonali =greet.charAt(8);
+console.log(sonali);
+
+console.log(greet.charAt(8));
+
+var char = greet.indexOf('O');
+console.log(char);
+
+console.log(greet.indexOf("bro"));
+console.log("=======================");
+console.log(greet.charAt(-2)); 
