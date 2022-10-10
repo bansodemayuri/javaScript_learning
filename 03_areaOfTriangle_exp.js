2ec701d6ba0f662240186039bc9e4cd75893f228
bansodemayuri <113650944+bansodemayuri@users.noreply.github.com>
2022-10-09 23:57:10 -1200

    Area of Triangle Assignment
    Area of Triangle Assignment

---
+++ 03_areaOfTriangle_exp.js
@@ -0,0 +1,10 @@
+var test = function(base, height,result){
+    console.log("Area of Triangle");
+    result = 1/2 * base * height;
+    return result;
+
+
+}
+var returnValue = test(40 , 32);
+
+console.log(returnValue);
