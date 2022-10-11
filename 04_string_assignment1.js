7e519301eb01fb989261f26e8e6d915ca684b7cb
bansodemayuri <113650944+bansodemayuri@users.noreply.github.com>
2022-10-11 00:05:04 -1200

    String Assingment1
    String Assingment1

---
+++ 04_string_assignment1.js
@@ -0,0 +1,15 @@
+console.log("============1.My Dream Company Name=============");
+console.log(`My Dream company is "Infosys"`);
+
+console.log("============2.My Hobbies=============");
+
+    var myHobby1 = "Reading";
+    var myHobby2 = "Traveling";
+    var myHobby3 = "Cooking";
+    console.log(myHobby1);
+    console.log(myHobby2);
+    console.log(myHobby3);
+
+    console.log("============Concat String in one line=============");
+    console.log(myHobby1+","+myHobby2+","+myHobby3 );
+
