0cccbb880e26cc1f5b819997cbfa0a4a72530d07
bansodemayuri <113650944+bansodemayuri@users.noreply.github.com>
2022-10-29 08:52:50 +0530

    11_array_objects
    11_array_objects

--- 11_array_objects.js
+++ 11_array_objects.js
@@ -21,3 +21,15 @@ for (let index = 0; index < array.length; index++) {
         console.log(` ${element.fullName}  ${element.city}  ${element.age} ${element.gender}  `);
     }
 }
+
+const arrayNumbers = [3,4, 56, 77, 88];
+for (const element of arrayNumbers) {
+    console.log(element);
+}
+
+for (const person of array) {
+    if(person.age>=50) {
+        console.log(` ${person.fullName}  ${person.city}  ${person.age} ${person.gender}  `);
+    }
+}
+
