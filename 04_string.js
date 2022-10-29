4f31f7d1c98ca9f047a683fa17a40cf7f7baeea2
bansodemayuri <113650944+bansodemayuri@users.noreply.github.com>
2022-10-29 08:51:41 +0530

    string
    string

--- 04_string.js
+++ 04_string.js
@@ -16,6 +16,7 @@ console.log(greet.charAt(8));
 var char = greet.indexOf('O');
 console.log(char);
 
+
 console.log(greet.indexOf("bro"));
 console.log("=======================");
 console.log(greet.charAt(-2));
