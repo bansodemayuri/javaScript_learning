27c8ea345cc76697948ac8f093ca69508affe7b7
bansodemayuri <113650944+bansodemayuri@users.noreply.github.com>
2022-11-01 07:54:28 +0530

    12_set
    12_set

--- 12_set.js
+++ 12_set.js
@@ -7,3 +7,27 @@ setOfNumbers.add(7);
 setOfNumbers.add(9);
 setOfNumbers.add(11);
 console.log(setOfNumbers);
+
+
+console.log("====== Adding duplicate element =======");
+setOfNumbers.add(7);
+console.log(setOfNumbers);
+
+console.log("====== Checking number elements available into set =======");
+console.log(setOfNumbers.size);
+
+
+console.log("====== Deleting element 9 =======");
+setOfNumbers.delete(9);
+console.log(setOfNumbers.size);
+console.log(setOfNumbers);
+
+console.log("====== Check whether element 7 available into set or not =======");
+let isAvailable = setOfNumbers.has(7);
+console.log(isAvailable);
+console.log(setOfNumbers.has(9));
+
+console.log("====== Traversing set =======");
+for (const element of setOfNumbers) {
+    console.log(element);
+}
