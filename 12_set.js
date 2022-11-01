0cdbac5e0566b32c28e73ae0df1da5582e53aeb9
bansodemayuri <113650944+bansodemayuri@users.noreply.github.com>
2022-11-01 07:19:13 +0530

    12_set
    12_set

---
+++ 12_set.js
@@ -0,0 +1,9 @@
+let setOfNumbers = new Set();
+console.log(typeof setOfNumbers);
+console.log("====== Adding element into the set collection=======");
+setOfNumbers.add(2);
+setOfNumbers.add(4);
+setOfNumbers.add(7);
+setOfNumbers.add(9);
+setOfNumbers.add(11);
+console.log(setOfNumbers);
